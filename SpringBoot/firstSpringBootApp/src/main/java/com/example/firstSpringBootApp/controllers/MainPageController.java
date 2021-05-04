27e0e8a34f4c5c342ce57d246ee28fd880e00fdc
zaxcon <luzin.alexandr@mail.ru>
2021-05-04 18:06:28 +0500

    Home Work 4.7

--- SpringBoot/firstSpringBootApp/src/main/java/com/example/firstSpringBootApp/controllers/MainPageController.java
+++ SpringBoot/firstSpringBootApp/src/main/java/com/example/firstSpringBootApp/controllers/MainPageController.java
@@ -31,10 +31,60 @@ public class MainPageController {
         return "genres/index";
     }
     @GetMapping("/authors")
-
     public String authorsPage(Model model)
     {
         model.addAttribute("authors",authorsService.getAuthors());
         return "authors/index";
+    }
+    @GetMapping("/signin")
+    public String signinPage(Model model)
+    {
+        return "signin";
+    }
+
+    @GetMapping("/signup")
+    public String signupPage(Model model)
+    {
+        return "signup";
+    }
+    @GetMapping("/contacts")
+    public String contactsPage(Model model)
+    {
+        return "contacts";
+    }
+    @GetMapping("/faq")
+    public String faqPage(Model model)
+    {
+        return "faq";
+    }
+    @GetMapping("/about")
+    public String aboutPage(Model model)
+    {
+        return "about";
+    }
+    @GetMapping("/documents")
+    public String documentsPage(Model model)
+    {
+        return "documents";
+    }
+    @GetMapping("/postponed")
+    public String postponedPage(Model model)
+    {
+        return "postponed";
+    }
+    @GetMapping("/cart")
+    public String cartPage(Model model)
+    {
+        return "cart";
+    }
+    @GetMapping("/recent")
+    public String recentPage(Model model)
+    {
+        return "books/recent";
+    }
+    @GetMapping("/popular")
+    public String popularPage(Model model)
+    {
+        return "books/popular";
     }
 }
